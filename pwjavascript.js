855ff8f49262c29b65e2b889174264a9984a2873
jenniferyrlee <jenniferyrlee@gmail.com>
2016-11-08 22:23:25 -0500

    JS for the Fixed Nav Bar
    Fixed the glitch in the fixed nav bar
    Implemented smooth scroll & slide down/up.

--- pwjavascript.js
+++ pwjavascript.js
@@ -1,15 +1,41 @@
 $(document).ready(function(){
 
   $(window).scroll(function() {
-    var showFixedNavBar = ($(document).scrollTop() > ($('#mainscreen').offset().top + $('#mainscreen').height()));
+    var showFixedNavBar = ($(document).scrollTop() >= ($('#mainscreen').offset().top + $('#mainscreen').height()));
     // $('#fixed-nav').css('display', showFixedNavBar ? '' : 'none')
 
     if (showFixedNavBar) {
-      $('#fixed-nav').css('display', '')      
+      $('#fixed-nav:hidden').slideDown();   
     } else {
-      $('#fixed-nav').css('display', 'none')
+      $('#fixed-nav:visible').slideUp();
     }
 
   });
 
+
+
+
+
+// Smooth scroll between divs
+
+$('a.smoothscroll').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 700);
+    }
 });
+
+
+
+
+
+ 
+});
+
+
+
